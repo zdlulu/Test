629a4239024970716a53ecd7dbf1e0736b166bfc
zdlulu <329433319@qq.com>
2017-07-04 14:33:38 +0800

    主文件中添加东西

--- HelloWorld/app/src/main/java/com/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/helloworld/MainActivity.java
@@ -2,12 +2,15 @@ package com.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        btn = (Button)findViewById(R.id.button);
     }
 }

5d7a29395e77fbd19f1314c5e528888c6836fab4
zdlulu <329433319@qq.com>
2017-07-04 14:35:49 +0800

    添加了一个other类

---
+++ HelloWorld/app/src/main/java/com/other/other.java
@@ -0,0 +1,14 @@
+package com.other;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/7/4.
+ */
+
+public class other {
+
+    public other(){
+        Log.i("pther","other");
+    }
+}
